a4d2a274fe3a5f4f86579f18edf66b99335b3bde
Bayron Vazquez <bayronnjonathan@gmail.com>
2025-01-21 13:35:25 -0600

    add service-card component for service section

---
+++ resources/views/components/theme/anyar/service-card.blade.php
@@ -0,0 +1,13 @@
+<div class="icon-box">
+    <div>
+        <i class="{{ $icono ?? '' }}"></i>
+        <h4 class="">
+            <a href="{{ $link ?? '#' }}">
+                {{ $titulo ?? '' }}
+            </a>
+        </h4>
+    </div>
+    <p>
+        {{ $descripcion ?? '' }}
+    </p>
+</div>
