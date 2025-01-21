94aa7cd02c10fa2a7dfbfda722cff630f3bb5faa
Bayron Vazquez <bayronnjonathan@gmail.com>
2025-01-20 21:04:06 -0600

    Add IconBox component with Blade template for displaying a cardbox

---
+++ resources/views/components/theme/anyar/icon-box.blade.php
@@ -0,0 +1,13 @@
+<div class="icon-box">
+    <div class="icon">
+        <i class="{{ $icono ?? '' }}"></i>
+    </div>
+    <h4 class="title">
+        <a href="{{ $link ?? '#' }}">
+            {{ $titulo ?? '' }}
+        </a>
+    </h4>
+    <p class="description">
+        {{ $descripcion ?? '' }}
+    </p>
+</div>
