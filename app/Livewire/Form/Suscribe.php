160e8aa8d2124d35e8926ef55a759d555d249162
Bayron Vazquez <bayronnjonathan@gmail.com>
2025-01-22 21:46:17 -0600

    Add template, migration, controller and livewire form for suscribe section

--- app/Livewire/Form/Suscribe.php
+++ app/Livewire/Form/Suscribe.php
@@ -4,8 +4,36 @@ namespace App\Livewire\Form;
 
 use Livewire\Component;
 
+use App\Models\Suscriptor;
+
 class Suscribe extends Component
 {
+    public ?string $email;
+
+    protected $rules = [
+        "email" => "required|email|max:200"
+    ];
+
+    public function save()
+    {
+        $data = $this->validate();
+        $this->reset();
+
+        $suscriptor = Suscriptor::where('email', $this->email)->fisrt();
+
+        if($suscriptor)
+        {
+            $suscriptor->active = true;
+            $suscriptor->save();
+        }
+        else
+        {
+            Suscriptor::insert($data);
+        }
+        $this->dispatch("alert-success", "Your email has been registered in our alert newsletter.");
+
+    }
+
     public function render()
     {
         return view('livewire.form.suscribe');
---
+++ database/migrations/2025_01_23_033514_create_suscriptors_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('suscriptors', function (Blueprint $table) {
+            $table->id();
+            $table->string('email',200)->unique();
+            $table->boolean('active')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('suscriptors');
+    }
+};
