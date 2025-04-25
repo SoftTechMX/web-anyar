3d7ba91af68320aae11d5fa130f545036c2fb432
Bayron Vazquez <bayronnjonathan@gmail.com>
2025-04-25 10:20:24 -0500

    actualizando componente de paginacion de bootstrap para hacerlo generico

--- resources/views/components/theme/bootstrap5/table/column/actions.blade.php
+++ resources/views/components/theme/bootstrap5/table/column/actions.blade.php
@@ -3,19 +3,19 @@
     <div class="w-100 d-flex justify-content-center">
 
         @if($btn_show == true)
-            <a href='{{ route("{$route}.show", $model->id) }}' class="btn btn-info btn-sm " title="{{ __('View') }}">
+            <a href='{{ route("{$route}.show", $model) }}' class="btn btn-info btn-sm " title="{{ __('View') }}">
                 <i class='bx bx-show'></i>
             </a>
         @endif
 
         @if($btn_edit == true)
-            <a href='{{ route("{$route}.edit", $model->id) }}' class="btn btn-warning btn-sm mx-1" title="{{ __('Edit') }}">
+            <a href='{{ route("{$route}.edit", $model) }}' class="btn btn-warning btn-sm mx-1" title="{{ __('Edit') }}">
                 <i class='bx bx-edit'></i>
             </a>
         @endif
 
         @if($btn_delete == true)
-            <form action='{{ route("{$route}.destroy", $model->id) }}' method="POST" style="display: inline-block;">
+            <form action='{{ route("{$route}.destroy", $model) }}' method="POST" style="display: inline-block;">
                 @csrf
                 @method('DELETE')
                 <button type="submit" class="btn btn-danger btn-sm" title="{{ __('Delete') }}" onclick="return confirm('{{ __('Are you sure you want to delete this?') }}')">
